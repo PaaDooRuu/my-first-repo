2b5b8508a29d1c97867af04a5cdf35e50420765c
PaaDooRuu <pichuev2020@mail.ru>
2026-06-24 02:36:42 +0900

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Этап 4.7 - Вывод сообщения в консоль
-console.log("Привет! Скрипт подключен и работает.");
-
-// Этап 4.10 - Вставка текущей даты в подвал
-const dateSpan = document.getElementById("update-date");
-const today = new Date();
-dateSpan.textContent = today.toLocaleDateString("ru-RU");
